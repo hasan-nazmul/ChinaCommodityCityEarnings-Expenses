c4a783ffc2e5ef7f17313e4fd1bbf69297391a47
hasan-nazmul <nh2826239@gmail.com>
2025-12-28 18:51:33 +0600

    admin id issue fixed

---
+++ store/management/commands/inituser.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+class Command(BaseCommand):
+    help = 'Creates a superuser if none exists'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        username = os.environ.get('SUPER_USER_NAME', 'admin')
+        password = os.environ.get('SUPER_USER_PASSWORD', 'admin123')
+        
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email='', password=password)
+            self.stdout.write(self.style.SUCCESS(f'Successfully created superuser: {username}'))
+        else:
+            self.stdout.write(self.style.SUCCESS('Superuser already exists. Skipping.'))
