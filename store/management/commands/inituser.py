8149c43300999013b7ea39ec76154d898ed902bd
hasan-nazmul <nh2826239@gmail.com>
2025-12-28 18:52:58 +0600

    admin id issue fixed

--- store/management/commands/inituser.py
+++ store/management/commands/inituser.py
@@ -7,8 +7,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         User = get_user_model()
-        username = os.environ.get('SUPER_USER_NAME', 'admin')
-        password = os.environ.get('SUPER_USER_PASSWORD', 'admin123')
+        username = os.environ.get('SUPER_USER_NAME', 'Nazmul')
+        password = os.environ.get('SUPER_USER_PASSWORD', 'N@zmul12345')
         
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email='', password=password)
